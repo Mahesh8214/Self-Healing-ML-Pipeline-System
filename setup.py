255f6694c5fe73ca4b0a336734a34051e2281c10
Mahesh8214 <maheshsingh8214@gmail.com>
2026-03-11 19:56:14 +0530

    updated

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str) -> List[str]:
         return requirements
 
 setup(
-    name="Practice DPP",
+    name="Practice-DiamondPP",
     version = "1.0.1",
     author = "Mahesh",
     author_email = "student.maheshsingh@gmail.com",
