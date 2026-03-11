fc0dfe2c4ac599a06a19d027ac09d922985fb6a8
Mahesh <129616353+Mahesh8214@users.noreply.github.com>
2026-03-11 17:36:02 +0000

    upate

--- notebooks/test_drift_data_maker.py
+++ notebooks/test_drift_data_maker.py
@@ -34,7 +34,7 @@ reference_data.to_csv("artifacts/data/reference_data.csv", index=False)
 test_data.to_csv("artifacts/data/test_data.csv", index=False)
 
 # Create production batches
-batch_size = 150
+batch_size = 300
 
 for i in range(0, len(production_data), batch_size):
     batch = production_data[i:i+batch_size]
