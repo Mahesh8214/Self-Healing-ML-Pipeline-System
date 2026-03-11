683d4693135b53c9c081b394b9b5f855e982772e
Mahesh <129616353+Mahesh8214@users.noreply.github.com>
2026-03-11 17:27:06 +0000

    added

--- notebooks/test_drift_data_maker.py
+++ notebooks/test_drift_data_maker.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import os
+import shutil  # Add this import
+
+# Delete previous folders (safe, ignores if missing)
+folders_to_clear = ["artifacts/data", "data/production_batches"]
+for folder in folders_to_clear:
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+        print(f"Deleted {folder}")
 
 # Load dataset
 df = pd.read_csv("data/raw/gemstone.csv")
@@ -17,7 +25,7 @@ reference_data = df[:train_end]
 test_data = df[train_end:test_end]
 production_data = df[test_end:]
 
-# Create folders
+# Create fresh folders
 os.makedirs("artifacts/data", exist_ok=True)
 os.makedirs("data/production_batches", exist_ok=True)
 
@@ -29,11 +37,8 @@ test_data.to_csv("artifacts/data/test_data.csv", index=False)
 batch_size = 150
 
 for i in range(0, len(production_data), batch_size):
-
     batch = production_data[i:i+batch_size]
-
     batch_id = i // batch_size + 1
-
     batch.to_csv(f"data/production_batches/batch_{batch_id}.csv", index=False)
 
-print("Dataset split completed.")
+print("Dataset split completed fresh.")
